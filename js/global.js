e43339ee14feffcb91d77b84983a52dba60acc02
Juraj Skripsky <js@hotfeet.ch>
2010-08-20 15:00:31 +0000

    HF-4: Make the footer stick to the window border if there is enough space.

---
+++ js/global.js
@@ -0,0 +1,29 @@
+$(window).load(function() {
+	positionFooter();
+});
+
+$(window).resize(function() {
+	positionFooter();
+});
+
+var footer;
+var lowerEdge;
+var lastResult;
+ 
+function positionFooter() {
+	if(!footer) {
+		footer = $("#Footer");
+		lowerEdge = $(footer).offset().top + $(footer).outerHeight(true);
+	}
+
+	var result = ($(window).height() > lowerEdge);
+	if(result == lastResult)
+		return;
+
+	if(result)
+		$(footer).addClass("sticking");
+	else
+		$(footer).removeClass("sticking");
+	
+	lastResult = result;	
+}
