d95c981f753ed6d7f660541744c25c5c96f7e674
Tom Lozza <tl@hotfeet.ch>
2010-10-01 16:41:03 +0000

    HF-14

--- src/AppManager.cs
+++ src/AppManager.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Net.Mail;
+using System.Text;
 
 namespace HotFeet.Website {
 	public class AppManager {
@@ -7,6 +9,17 @@ namespace HotFeet.Website {
 		public Database DB {
 			get { return db; }
 			set { db = value; }
+		}
+		
+		public void SendEmail(string from, string to, string subject, string body) {
+			
+			MailMessage msg = new MailMessage(from, to, subject, body) {
+				BodyEncoding = Encoding.GetEncoding("ISO-8859-1"),
+				SubjectEncoding = Encoding.GetEncoding("ISO-8859-1")
+			};
+			
+			SmtpClient client = new SmtpClient("localhost");
+			client.Send(msg);
 		}
 	}
 }
