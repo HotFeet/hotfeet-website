f1d8eeee38b4c84780755e16aee29f8a80aa73c1
Juraj Skripsky <js@hotfeet.ch>
2010-10-21 12:36:37 +0000

    HF-2: jQuery plugin clean-ups

--- js/jquery.couple.js
+++ js/jquery.couple.js
@@ -1,20 +1,22 @@
- jQuery.fn.couple = function(target, options) {
- 	 options = jQuery.extend({
- 	 	hoverClass: "selected",
- 	 	events: "hover click"
-	}, options);
-
-	if(options.events.indexOf("hover") != -1) {
-		$(this).hover(
+(function($) {
+	$.fn.couple = function(target, options) {
+		options = jQuery.extend({
+			hoverClass: "selected",
+			events: "hover click"
+		}, options);
+	
+		if(options.events.indexOf("hover") != -1) {
+			$(this).hover(
 				function() { $(target).toggleClass(options.hoverClass, true); },
 				function() { $(target).toggleClass(options.hoverClass, false); }
-		);
-		var source = this;
-		$(target).hover(
+			);
+			var source = this;
+			$(target).hover(
 				function() { $(source).toggleClass(options.hoverClass, true); },
 				function() { $(source).toggleClass(options.hoverClass, false); }
-		);
+			);
+		}
+		if(options.events.indexOf("click") != -1)
+			$(this).click(function() { $(target).click(); });
 	}
-	if(options.events.indexOf("click") != -1)
-		$(this).click(function() { $(target).click(); });
-};
+})(jQuery);
--- js/jquery.fadeSliderToggle.js
+++ js/jquery.fadeSliderToggle.js
@@ -8,24 +8,18 @@
  * Licenced under the MIT Licence
  */
 
-
- jQuery.fn.fadeSliderToggle = function(settings) {
- 	/* Damn you jQuery opacity:'toggle' that dosen't work!~!!!*/
- 	 settings = jQuery.extend({
-		speed:500,
-		easing : "swing"
-	}, settings)
-	
-	caller = this
- 	if($(caller).css("display") == "none"){
- 		$(caller).animate({
- 			opacity: 1,
- 			height: 'toggle'
- 		}, settings.speed, settings.easing);
-	}else{
-		$(caller).animate({
- 			opacity: 0,
- 			height: 'toggle'
- 		}, settings.speed, settings.easing);
+(function($) {
+	$.fn.fadeSliderToggle = function(settings) {
+	 	/* Damn you jQuery opacity:'toggle' that dosen't work!~!!!*/
+	 	 settings = jQuery.extend({
+			speed:500,
+			easing : "swing"
+		}, settings);
+		
+		$(this).animate({
+			opacity: ($(this).css("display") == "none" ? 1 : 0),
+			height: 'toggle'
+		}, settings.speed, settings.easing);
 	}
-}; 
+})(jQuery);
+ 
