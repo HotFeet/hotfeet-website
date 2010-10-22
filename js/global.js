98f6a6e4fc5c8147114e11ba5df05e7969d47dcf
Juraj Skripsky <js@hotfeet.ch>
2010-10-22 14:52:18 +0000

    HF-2

--- js/global.js
+++ js/global.js
@@ -3,16 +3,20 @@ $(document).ready(function(){
 		$("body").addClass("browser-ie").addClass("browser-ie" + $.browser.version.substring(0, 1));
 	}
 
+	// add vertical separator bars to navigation
 	addSeparators($("#MainNavigation li"));
 	addSeparators($("#MainSubNavigation li"));
 	addSeparators($("ServiceNavigation li:not(.action)"));
 
+	// nice HotFeet-style borders
 	$("#Body h2").backgroundBorder();
 
+	// folded texts
 	$("a.more-info").click(function() {
 		$(this).next("div.details").fadeSliderToggle();
 	});
 
+	// textbox watermarks
 	$("input[type='text'], textarea").each(function() {
 		var title = $(this).attr("title");
 		var carValue = $(this).val();
@@ -22,7 +26,30 @@ $(document).ready(function(){
 
 	//TODO: delay this:
 	$.localScroll.hash({easing:'swing'});
+	
+	setupPopupOverlay();
 });
+
+function setupPopupOverlay() {
+	$("a.popup").overlay({
+		top: "center",
+		mask: {
+			color: '#202020',
+			loadSpeed: 500,
+			opacity: 0.2
+		},
+		onBeforeLoad: function() {
+			var trigger = this.getTrigger();
+			var overlay = this.getOverlay();
+
+			var src = trigger.attr("href");
+			var iframe = overlay.find("iframe");
+			// don't set the url if it's already done
+			if(iframe.attr("src") != src)
+				iframe.attr("src", src);
+		}
+	});
+}
 
 function addSeparators(elements) {
 	$(elements.not(":last")).append("|");
