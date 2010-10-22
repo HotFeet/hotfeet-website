a4887675501b9acd8d4bfbca1f1d6996ce9cb873
Tom Lozza <tl@hotfeet.ch>
2010-10-22 14:33:36 +0000

    HF-19:Overloaded email sending func for convenience

--- src/AppManager.cs
+++ src/AppManager.cs
@@ -4,6 +4,8 @@ using System.Text;
 
 namespace HotFeet.Website {
 	public class AppManager {
+		const string hfInfoEmail = "info@hotfeet.ch"; 
+		const string hfSupportEmail = "support@hotfeet.ch";
 		Database db;
 		
 		public Database DB {
@@ -11,13 +13,19 @@ namespace HotFeet.Website {
 			set { db = value; }
 		}
 		
+		public void SendEmailToHfInfo(string from, string subject, string body) {
+			SendEmail(from, hfInfoEmail, subject, body);
+		}
+		
+		public void SendEmailToHfSupport(string from, string subject, string body) {
+			SendEmail(from, hfSupportEmail, subject, body);
+		}
+		
 		public void SendEmail(string from, string to, string subject, string body) {
-			
 			MailMessage msg = new MailMessage(from, to, subject, body) {
 				BodyEncoding = Encoding.GetEncoding("ISO-8859-1"),
 				SubjectEncoding = Encoding.GetEncoding("ISO-8859-1")
 			};
-			
 			SmtpClient client = new SmtpClient("localhost");
 			client.Send(msg);
 		}
